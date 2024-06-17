36d6c4311b636ed32f1e90b48826b5df4ae8ffa9
yash tiwary <89058607+kyt1s@users.noreply.github.com>
2024-06-17 16:35:06 +0530

    changes

--- Pariksha.com/src/App.jsx
+++ Pariksha.com/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Index from './Components/Index'
+import Index from './Components/Home'
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import Createuser from './Components/Createuser'
 import Users from './Components/Users'
@@ -12,14 +12,7 @@ function App() {
   return (
     <>
    <div>
-    <BrowserRouter>
-    <Index/>
-    <Routes>
-      <Route element={<Createuser/>} path="/"/>
-      <Route element={<Users/>} path="/users"/>
-      <Route element={<Editusers/>} path="/edit"/>
-    </Routes>
-    </BrowserRouter>
+    <Home/>
    </div>
    </>
   )
---
+++ Pariksha.com/src/Components/Home.jsx
@@ -0,0 +1,6 @@
+const Home =()=>{
+  return(
+    <h1>This is home page</h1>
+  )
+}
+export default Home
